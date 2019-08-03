aa31ab56e0843ed86e40bb48d15da52622e4a66f
Alex Martin <atonymartin20@yahoo.com>
2019-08-02 22:51:16 -0500

    Added everything but main-content section

--- js/index.js
+++ js/index.js
@@ -63,4 +63,52 @@ let navItem6 = document.querySelectorAll('a')[5];
 navItem6.innerHTML = siteContent["nav"]["nav-item-6"];
 
 
-//   CTA
+// CTA
+let ctaH1 = document.querySelector('h1');
+// ctaH1.innerHTML = siteContent["cta"]["h1"]
+ctaH1.innerHTML = "DOM<br> Is<br> Awesome";
+
+let button = document.querySelector('button');
+button.innerHTML = siteContent["cta"]["button"];
+
+let ctaIMG = document.getElementById("cta-img");
+ctaIMG.setAttribute('src', siteContent["cta"]["img-src"]);
+
+
+// "main-content": {
+//   "features-h4":"Features",
+//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "about-h4":"About",
+//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "middle-img-src": "img/mid-page-accent.jpg",
+//   "services-h4":"Services",
+//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "product-h4":"Product",
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "vision-h4":"Vision",
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// },
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
+
+// Contact
+let contactH4 = document.querySelectorAll('h4')[5];
+contactH4.innerHTML = siteContent["contact"]["contact-h4"];
+
+let address = document.querySelectorAll('p')[5];
+// address.innerHTML = siteContent["contact"]["address"];
+address.innerHTML = "123 Way 456 Street <br /> Somewhere, USA";
+
+let phone = document.querySelectorAll('p')[6];
+phone.innerHTML = siteContent["contact"]["phone"];
+
+let email = document.querySelectorAll('p')[7];
+email.innerHTML = siteContent["contact"]["email"];
+
+// Footer
+let footer = document.querySelector('footer');
+footer.innerHTML = siteContent["footer"]["copyright"];
